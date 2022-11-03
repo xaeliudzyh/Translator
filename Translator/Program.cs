5fa2fb7739645b91696fce0cc142ee200e2a57cb
Tsagolov George Zaurbekovich <gtsagolov8@gmail.com>
2022-11-03 13:36:46 +0300

    made huge changes

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -5,9 +5,9 @@ using System.Text;
 namespace Translator
 {
 
-
     class Program
     {
+
         //считываем файл
         public static string[] ReadFile(string fileName)
         {
@@ -15,32 +15,45 @@ namespace Translator
             return initialCode;
         }
 
+
+
         //записываем в файл
         public static void WriteFile(string fileName, string[] translatedCode)
         {
             File.WriteAllLines(fileName, translatedCode);
         }
 
-            public static Dictionary<string, string> data = new Dictionary<string, string>()
-            {           {"unsigned long long", "ulong"},
-                        {"unsigned long", "uint" },
-                        {"unsigned short", "ushort" },
-                        {"long long" , "long"},
-                        {"long", "int" }
-            };
+
+
+        public static Dictionary<string, string> data = new Dictionary<string, string>()
+        {
+            {"unsigned long long", "ulong"},
+
+            {"unsigned long", "uint" },
+
+            {"unsigned short", "ushort" },
+
+            {"long long" , "long"},
+
+            {"long", "int" }
+        };
+
+
+
         //меняем содержимое(собственно процесс ре-транляции)
         public static string[] VarChanger(string file)
         {
-            
 
             //изначальный код(на плюсах)
             string[] initialString = ReadFile(file);
+
+
             for(int i = 0; i < initialString.Length; i++)
             {
                 string line = initialString[i];
-                //string[] subs = line.Split(' ');
-
-                if (line.Contains("#include") || line.Contains("namespace"))
+              
+                // убираем инклуды и пространства имен
+                if (line.Contains("#include") || line.Contains("namespace") || line.Contains("int main()"))
                 {
                     initialString[i] = "";
                     continue;
@@ -49,17 +62,16 @@ namespace Translator
                 // "переводим" считывание с клавиатуры
                 if(line.Contains("cin>>"))
                 {
-                    line.Replace("cin>>", "Console.ReadLine(");
+                    line = line.Replace("cin>>", "Console.ReadLine(");
                     while (Regex.Matches(line, ">>").Count > 0)
                         line = line.Replace(">>", ",");
-                    line = line.Replace(";", ");");
-                  
+                    line = line.Replace(";", ");");                
                 }
 
                 // "переводим" вывод с консоли
                 if (line.Contains("cout<<"))
                 {
-                    line.Replace("cout<<", "Console.WriteLine(");
+                    line = line.Replace("cout<<", "Console.WriteLine(");
                     while (Regex.Matches(line, "<<").Count > 0)
                         line = line.Replace("<<", ",");
                     line = line.Replace(";", ");");
@@ -72,104 +84,32 @@ namespace Translator
                     if (line.Contains(key))
                     {
                         line = line.Replace(key, data[key]);
-
                     }
                 }
-
-                /*if(h.Contains("unsigned long long"))
-                {
-                    while (Regex.Matches(h, "unsigned long long").Count > 0)
-                        h.Replace("unsigned long long", data["unsigned long long"]);
-
-                }
-
-                if (h.Contains("unsigned long"))
-                {
-                    while (Regex.Matches(h, "unsigned long").Count > 0)
-                        h.Replace("unsigned long", data["unsigned long"]);
-
-                }
-
-                if (h.Contains("unsigned short"))
-                {
-                    while (Regex.Matches(h, "unsigned short").Count > 0)
-                        h.Replace("unsigned short", data["unsigned short"]);
-
-                }
-
-
-                for (int j = 0; j < subs.Length; j++)
-                {
-                    if (subs[j] == "unsigned")
-                    {
-                        if (subs[j + 1] == "short")
-                        {
-                            subs[j] = "ushort";
-                            subs[j + 1] = "";
-                        }
-                        else
-                        {
-                            if (subs[j + 1] == "long" && subs[j + 2] == "long")
-                            {
-                                subs[j] = "ulong";
-                                subs[j + 1] = "";
-                                subs[j + 2] = "";
-                                j++;
-                            }
-                            else
-                            {
-                                if (subs[j + 1] == "long")
-                                {
-                                    subs[j] = "uint";
-                                    subs[j + 1] = "";
-                                }
-                            }
-                        }
-                        j++;
-                    }
-                    else
-                    {
-                        if (subs[j]=="long" && subs[j + 1] == "long")
-                        {
-                            subs[j + 1] = "";
-                            j++;
-                        }
-                        else
-                            if (data.ContainsKey(subs[j]))
-                                subs[j] = data[subs[j]];
-
-                    }
-                }
-                string res = "";
-                for (int z = 0; z < subs.Length; z++)
-                {
-                    res += subs[z];
-                    res += " ";
-                }
-                initialString[i] = res;*/
                 initialString[i] = line;
-
             }
-          
-
             return initialString;
-
         }
 
 
 
-
-
-        static void Main(string[] fileName)
+        static void Main(string[] fileNames)
         {
-            string[] j=VarChanger("../../../data/reTranslated1.txt");
-            Console.WriteLine("namespace Namespace{\n tclass Program{ \n   static void Translator(string[] args){\n");
-            for (int i = 0; i < j.Length; i++)
+            string initialFile = fileNames[0];
+            //string destinationFile = fileNames[1];
+            string[] changedCode= VarChanger(initialFile);
+            string finalCode = "";
+
+            finalCode+="namespace Namespace{\n class Program{ \n   static void Main(string[] args){\n";
+            for (int i = 0; i < changedCode.Length; i++)
             {
-                Console.Write("       ");
-                Console.WriteLine(j[i]);
+                finalCode+="        ";
+                finalCode+=changedCode[i];
+                finalCode += "\n";
             }
-            Console.WriteLine("}");
+            finalCode+="} \n }";
+            Console.WriteLine(finalCode);
+            
         }
     }
        
